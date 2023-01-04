c03a5e8941fce20834d447b7e7abf887a38710d6
Martal85 <mraleksiev85@gmail.com>
2023-01-04 17:10:23 +0200

    update commands
    added missing command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('OpenHomePage', () => {  
+    cy.visit('https://checkout-staging.wheelbasepro.com/r/reserve?owner_id=28880')
+    cy.url().should('eq', 'https://checkout-staging.wheelbasepro.com/r/reserve?owner_id=28880')      
+    })
+
+ 
